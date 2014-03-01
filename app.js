ca328523a9f8ce68adce9857ace17257700dd13b
GarrisonK <gkeller572@gmail.com>
2014-02-28 23:31:35 -0600

    Added communication with sockets and basic buttons
    Only bet lock/unlock are listening for clicks/will get a response from
    the server. Messages appear in the bottom half of the main window.

--- app.js
+++ app.js
@@ -1,13 +1,42 @@
 var http = require('http'),
 	sio = require('socket.io'),
-	index = require('fs').readFileSync(__dirname+'/index.html')
+	index = require('fs').readFileSync(__dirname+'/index.html'),
+	fs = require('fs'),
+	url = require('url');
 
 
 var app = http.createServer(function(req,res){
-	res.writeHead(200,{'content-Type':'text/html'});
-	res.end(index);
+	var request = url.parse(req.url,true);
+	var action = request.pathname;
+	if(action == '/style.css'){
+		var style = fs.readFileSync('./style.css');
+		res.writeHead(200,{'Content-Type':'text/css'});
+		res.end(style,'binary');
+	}
+	else{
+		res.writeHead(200,{'content-Type':'text/html'});
+		res.end(index);
+	}
 }), io=sio.listen(app);
 
 app.listen(3000);
+
+io.sockets.on('connection',function(socket){
+
+	socket.emit("connection acknowledgment");
+
+	socket.on('disconnect',function(){
+
+	});
+
+	socket.on('bet locked',function(){
+		socket.emit('bet lock confirm');
+	});
+
+	socket.on('bet unlocked',function(){
+		socket.emit('bet unlock confirm');
+	});
+
+});
 
 
