c3e533420c394bdbb8ef46c3ff9e22b3ff6bf5b3
Robert Wooley <dudude@neo.tamu.edu>
2014-03-18 16:05:32 -0500

    Added support for watch Grunt task
    Now can run "grunt watch" from the command line so that unit tests will automatically be run once a source file is updated.

--- Gruntfile.js
+++ Gruntfile.js
@@ -1,31 +1,22 @@
 module.exports = function(grunt) {
-	grunt.initConfig({
-		pkg: grunt.file.readJSON('package.json'), // the package file to use
-		// qunit: {
-		// 	all: ['tests/*.html']
-		// }
-	// 	qunit: {
-	// 		all: {
-	// 			options: {
-	// 				urls: [
-	// 					'http://localhost:3000/tests/index.html',
-	// 				]
-	// 			}
-	// 		}
-	// 	}
-	// });
-		qunit: {
-			all: {
-				options: {
-					timeout: 5000,
-					urls: [
-						'tests/index.html'
-					]
-				}
-			}
-		}
-	});
-
-	grunt.registerTask('default', ['qunit']);
-	grunt.loadNpmTasks('grunt-contrib-qunit');
+    grunt.initConfig({
+        pkg: grunt.file.readJSON('package.json'), // the package file to use
+        watch: {
+            files: ['*.js', 'tests/*.html', 'tests/*.js'],
+            tasks: ['qunit']
+        },
+        qunit: {
+            all: {
+                options: {
+                    timeout: 5000,
+                    urls: [
+                        'tests/index.html'
+                    ]
+                }
+            }
+        }
+    });
+    grunt.loadNpmTasks('grunt-contrib-watch');
+    grunt.loadNpmTasks('grunt-contrib-qunit');
+    grunt.registerTask('default', ['qunit']);
 };
