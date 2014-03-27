f7ed8f0d6b1ecb0ad0c14af09bc16be137661c04
Robert Wooley <dudude@neo.tamu.edu>
2014-03-27 18:18:48 -0500

    First working client test
    Client testing code can now call functions within client.js.

--- tests/client_test.js
+++ tests/client_test.js
@@ -5,3 +5,7 @@ test("truthy", function() {
   equal(1, true, "1 is truthy");
   notEqual(0, true, "0 is NOT truthy");
 });
+
+test("isPair", function() {
+	ok(isPair(0,1), "First two tiles makes a pair");
+});
